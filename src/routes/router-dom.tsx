5efff14f2f0d74f7df8fd736b1b8b139c68280b8
Aditya Sharma <devganaditya@gmail.com>
2024-09-07 18:32:56 +0530

    Created Patient Sign In and Patient Registration Component.

--- src/routes/router-dom.tsx
+++ src/routes/router-dom.tsx
@@ -1,9 +1,25 @@
+import UserAppointment from "@/private/UserAppointment";
+import UserProfile from "@/private/UserProfile";
+import UserRegister from "@/public/UserRegister";
+import UserSignIn from "@/public/UserSignIn";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <></>,
+    element: <UserSignIn />,
+  },
+  {
+    path: "/register",
+    element: <UserRegister />,
+  },
+  {
+    path: "/profile",
+    element: <UserProfile />,
+  },
+  {
+    path: "/appointment",
+    element: <UserAppointment />,
   },
 ]);
 
