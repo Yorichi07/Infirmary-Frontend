62687813bb46e03e1da2f3e7cbcca2ac7267cf0f
Priyanshu Butola <priyanshubutola@gmail.com>
2024-09-08 21:04:46 +0530

    Created Doctor Dashboard

--- src/routes/router-dom.tsx
+++ src/routes/router-dom.tsx
@@ -1,3 +1,7 @@
+import AssistantDoctorDashboard from "@/private/assistant-dashboard/doctor-dashboard/AssistantDoctorDashboard";
+import AssistantDoctorDashboardLayout from "@/private/assistant-dashboard/doctor-dashboard/AssistantDoctorDashboardLayout";
+import DoctorDashboard from "@/private/doctor-dashboard/DoctorDashboard";
+import DoctorDashboardLayout from "@/private/doctor-dashboard/DoctorDashboardLayout";
 import StudentDasboardLayout from "@/private/student-dashboard/StudentDasboardLayout";
 import StudentDashboard from "@/private/student-dashboard/StudentDashboard";
 import { createBrowserRouter } from "react-router-dom";
@@ -16,6 +20,22 @@ const router = createBrowserRouter([
         element: <StudentDashboard />,
       },
     ],
+  },
+  {
+    path: "/doctor-dashboard",
+    element: (
+      <DoctorDashboardLayout>
+        <DoctorDashboard />
+      </DoctorDashboardLayout>
+    ),
+  },
+  {
+    path: "/assistant-doctor-dashboard",
+    element: (
+      <AssistantDoctorDashboardLayout>
+        <AssistantDoctorDashboard />
+      </AssistantDoctorDashboardLayout>
+    ),
   },
 ]);
 
