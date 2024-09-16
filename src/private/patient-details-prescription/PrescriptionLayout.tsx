381b4e2ddfc9cee39c88b3349f865590feb683a9
Divyanshu <sainidiv2003@gmail.com>
2024-09-16 14:04:14 +0530

    patient details

---
+++ src/private/patient-details-prescription/PrescriptionLayout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "@/components/Navbar";
+import Shared from "@/Shared";
+import React from "react";
+
+const PrescriptionLayout = ({ children }: { children: React.ReactNode }) => {
+  const navsetting = {
+    title: "Patient Details",
+    titleLogo: Shared.Prescription,
+    additionalLogo: Shared.ArrowLeft,
+    menu: false,
+    role: localStorage.getItem("roles"),
+  };
+  return (
+    <div className="h-screen">
+      <Navbar props={navsetting} />
+      {children}
+      <div className="w-full bg-white h-[8%] flex items-center justify-center text-[#8F8F8F] border-t  border-[gray] font-semibold">
+        Bidholi Campus: Tel: +91-135-2770137, 2776053, 2776054, 2776091 &nbsp;
+        &nbsp; &nbsp; Kandoli Campus: Tel: +91-135-2770137, 2776053, 2776054,
+        2776091
+      </div>
+    </div>
+  );
+};
+
+export default PrescriptionLayout;
