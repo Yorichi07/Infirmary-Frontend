cd6160136b6549cfa5365c139b87d9cd21850ae2
Lakshit <lakshitlucky11@gmail.com>
2024-12-10 13:50:56 +0530

    final changes

--- src/public/UserRegister.tsx
+++ src/public/UserRegister.tsx
@@ -151,7 +151,7 @@ const UserRegister = () => {
         };
 
         await axios
-          .post("http://192.168.176.1:8081/api/auth/patient/signup", payload)
+          .post("http://ec2-13-127-221-134.ap-south-1.compute.amazonaws.com/api/auth/patient/signup", payload)
           .then((res) => {
             return res.data;
           });
