5d6c81a359971da3d94a2c26bca95bee033b79de
Lakshit <lakshitlucky11@gmail.com>
2024-12-21 21:35:11 +0530

    final changes

--- src/private/Patient-Logs/PatientLogs.tsx
+++ src/private/Patient-Logs/PatientLogs.tsx
@@ -34,7 +34,7 @@ const PatientLogs = () => {
 
   const fetchData = async (date?: string) => {
     try {
-      let apiUrl = "http://localhost:8081/api/AD/getAppointmentByDate";
+      let apiUrl = "http://ec2-3-110-204-139.ap-south-1.compute.amazonaws.com/api/AD/getAppointmentByDate";
 
       if (date) {
         apiUrl += `?date=${date}`;
@@ -52,7 +52,7 @@ const PatientLogs = () => {
         reportId: rept.appointmentId,
         patientName: rept.PatientName,
         token: rept.token,
-        downloadLink: `http://localhost:8081/prescription?id=${rept.appointmentId}`,
+        downloadLink: `http://ec2-3-110-204-139.ap-south-1.compute.amazonaws.com/prescription?id=${rept.appointmentId}`,
       }));
 
       setReports(formatData);
